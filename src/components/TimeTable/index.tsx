24676634e0f417135f733ab0ca54270c7b97ce9c
Mykola Prymak <mykola.prymak@gmail.com>
2021-08-07 02:47:33 +0200

    feat: main logick

--- src/components/TimeTable/index.tsx
+++ src/components/TimeTable/index.tsx
@@ -1,8 +1,9 @@
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import { TimeTable as TimeTableType, WeekDayItem } from '../../constants/interfaces';
 import './index.scss';
+import { getDailyTimeTable, formatTime, getNextWeekDayKey, getOpenTimeIdx, getCloseTimeIdx } from './helpers';
 
-//
+// Weekdays title/keys
 const WEEK_DAYS: WeekDayItem[] = [
   {
     title: 'Monday',
@@ -35,10 +36,50 @@ const WEEK_DAYS: WeekDayItem[] = [
 ];
 
 const TimeTableItem = (props: { weekDay: WeekDayItem; timetable: TimeTableType; isToday: boolean }) => {
-  const { weekDay, isToday } = props;
+  const { weekDay, timetable, isToday } = props;
+
   // Sort current day times
-  // Find open one - if not - the day is Closed
-  // if today - show label
+  const dayTimeTable = getDailyTimeTable(weekDay.key, timetable);
+
+  // Array with open hours pairs
+  const openHours = [];
+  // Current position in dayTimeTable
+  let cursor;
+
+  // Find open one (on current cursor position) - if can't - no more open slots
+  // If we will have open records - day isn't closed
+  do {
+    cursor = getOpenTimeIdx(dayTimeTable, cursor);
+
+    // If we can't find open time - end the search
+    if (cursor === -1) {
+      break;
+    }
+    const openTime = dayTimeTable[cursor].value;
+
+    let closeTime = 0; // If we won't find the close time
+    // we assume that the store will close at 12 AM
+    cursor = getCloseTimeIdx(dayTimeTable, cursor);
+
+    // We have close time in this day
+    if (cursor !== -1) {
+      closeTime = dayTimeTable[cursor].value;
+    } else {
+      // Try to find close time in next day
+      // The close time should be the first one in sorted array
+      const nextDayTimeTable = getDailyTimeTable(getNextWeekDayKey(weekDay.key), timetable);
+      const nextDayFirstCloseIdx = getCloseTimeIdx(nextDayTimeTable);
+
+      if (nextDayFirstCloseIdx === 0) {
+        closeTime = nextDayTimeTable[nextDayFirstCloseIdx].value;
+      }
+    }
+
+    // Save our pair
+    openHours.push([openTime, closeTime]);
+
+    // Continue until we have results
+  } while (cursor !== -1);
 
   return (
     <div className="TimeTable-weekday">
@@ -46,13 +87,23 @@ const TimeTableItem = (props: { weekDay: WeekDayItem; timetable: TimeTableType; 
         {weekDay.title}
         {isToday && <label className="TimeTable-label">Today</label>}
       </div>
-      <div className="TimeTable-weekday-workingHours TimeTable-weekday-workingHours--none">Closed</div>
+      {openHours.length === 0 ? (
+        <div className="TimeTable-weekday-workingHours TimeTable-weekday-workingHours--none">Closed</div>
+      ) : (
+        <div className="TimeTable-weekday-workingHours">
+          {openHours.map(timePair => (
+            <span className="TimeTable-timeSlot" key={timePair[0]}>
+              <span>{formatTime(timePair[0])}</span> - <span>{formatTime(timePair[1])}</span>
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 const TimeTable = (props: { timetable: TimeTableType }) => {
-  const { timetable } = props;
+  const { timetable = {} } = props;
   // Get current day number 0-6 to determine today label
   // Because 0 represents Sunday we need to do shift
   let currentDayIdx = new Date().getDay() - 1;
---
+++ src/components/TimeTable/tests/index.test.tsx
@@ -0,0 +1,153 @@
+import { render, screen } from '@testing-library/react';
+
+import { TimeTable as TimeTableType } from '../../../constants/interfaces';
+import TimeTable from '../index';
+
+// Open just one day in the week
+const oneDayTimeTable: TimeTableType = {
+  monday: [
+    { type: 'open', value: 32400 },
+    { type: 'close', value: 72000 },
+  ],
+};
+
+// Opens only one day and closed on next
+const nextDayCloseTimeTable: TimeTableType = {
+  monday: [
+    {
+      type: 'close',
+      value: 3600,
+    },
+  ],
+  sunday: [
+    {
+      type: 'open',
+      value: 64800,
+    },
+  ],
+};
+
+// Closing on next day and have next day time slots
+const nextDayCloseAndOpenTimeTable: TimeTableType = {
+  friday: [
+    {
+      type: 'open',
+      value: 64800,
+    },
+  ],
+  saturday: [
+    {
+      type: 'close',
+      value: 3600,
+    },
+    {
+      type: 'open',
+      value: 32400,
+    },
+    {
+      type: 'close',
+      value: 39600,
+    },
+    {
+      type: 'open',
+      value: 57600,
+    },
+    {
+      type: 'close',
+      value: 82800,
+    },
+  ],
+};
+
+// Full data formatting example
+const fullWeekTimeTable: TimeTableType = {
+  monday: [],
+  tuesday: [
+    { type: 'open', value: 36000 },
+    { type: 'close', value: 64800 },
+  ],
+  wednesday: [],
+  thursday: [
+    { type: 'open', value: 36000 },
+    { type: 'close', value: 64800 },
+  ],
+  friday: [{ type: 'open', value: 36000 }],
+  saturday: [
+    { type: 'close', value: 3600 },
+    { type: 'open', value: 36000 },
+  ],
+  sunday: [
+    { type: 'close', value: 3600 },
+    { type: 'open', value: 43200 },
+    { type: 'close', value: 75600 },
+  ],
+};
+
+// Multislot day
+const multislotWeekTimeTable: TimeTableType = {
+  monday: [],
+  tuesday: [
+    { type: 'open', value: 3600 },
+    { type: 'close', value: 7200 },
+    { type: 'open', value: 36000 },
+    { type: 'close', value: 39800 },
+    { type: 'open', value: 43200 },
+    { type: 'close', value: 75600 },
+  ],
+};
+
+describe('<TimeTable />', () => {
+  let getDay = Date.prototype.getDay;
+
+  beforeEach(() => {
+    // Mock new Date().getDay() to prevent changing the today label
+    Date.prototype.getDay = () => 6; // We will have always Saturday
+  });
+
+  afterAll(() => {
+    Date.prototype.getDay = getDay;
+  });
+
+  it('renders default all closed timetable if nothing is passed', () => {
+    const { container } = render(<TimeTable />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders today label at Sunday', () => {
+    Date.prototype.getDay = () => 0;
+    const { container } = render(<TimeTable />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders one open day in the week', () => {
+    const { container } = render(<TimeTable timetable={oneDayTimeTable} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders properly if close time is on next day', () => {
+    const { container } = render(<TimeTable timetable={nextDayCloseTimeTable} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders properly if close time is on next day and we have time slots on next day', () => {
+    const { container } = render(<TimeTable timetable={nextDayCloseAndOpenTimeTable} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  
+  it('renders properly if we have full week of time slots', () => {
+    const { container } = render(<TimeTable timetable={fullWeekTimeTable} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders properly if we have couple of time slots in one day', () => {
+    const { container } = render(<TimeTable timetable={multislotWeekTimeTable} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
