83746cd1521442c79eaccfa886cf63247b352fe5
Mykola Prymak <mykola.prymak@gmail.com>
2021-08-07 18:48:42 +0200

    feat: add examples

--- src/App.js
+++ src/App.js
@@ -1,64 +1,160 @@
-import logo from './logo.svg';
 import './App.css';
-
 import TimeTable from './components/TimeTable';
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
-    font-family: 'Roboto', sans-serif;
-*/
 
-const timetable = {
-  monday: [
-  
-  { type: 'open', value: 3600 },
-  { type: 'close', value: 7200 },
-  { type: 'open', value: 36000 },
-  { type: 'close', value: 39800 },
-  { type: 'open', value: 43200 },
-  { type: 'close', value: 75600 },
-  
-],
-  tuesday: [
-    { type: 'open', value: 36000 },
-    { type: 'close', value: 64800 },
+const timetables = [
+  [
+    'Open just one day in the week',
+    {
+      monday: [
+        { type: 'open', value: 32400 },
+        { type: 'close', value: 72000 },
+      ],
+    },
   ],
-  wednesday: [
-    
-    { type: 'open', value: 37800 },
-    { type: 'close', value: 41400 },
-    
+
+  [
+    'Opens only one day and closed on next',
+    {
+      monday: [
+        {
+          type: 'close',
+          value: 3600,
+        },
+      ],
+      sunday: [
+        {
+          type: 'open',
+          value: 64800,
+        },
+      ],
+    },
   ],
-  thursday: [
-    { type: 'open', value: 36000 },
-    { type: 'close', value: 64800 },
+
+  [
+    'Closing on next day and have next day time slots',
+    {
+      friday: [
+        {
+          type: 'open',
+          value: 64800,
+        },
+      ],
+      saturday: [
+        {
+          type: 'close',
+          value: 3600,
+        },
+        {
+          type: 'open',
+          value: 32400,
+        },
+        {
+          type: 'close',
+          value: 39600,
+        },
+        {
+          type: 'open',
+          value: 57600,
+        },
+        {
+          type: 'close',
+          value: 82800,
+        },
+      ],
+    },
   ],
-  friday: [{ type: 'open', value: 36000 }],
-  saturday: [
-    { type: 'close', value: 3600 },
-    { type: 'open', value: 36000 },
+
+  [
+    'Full data formatting example',
+    {
+      monday: [],
+      tuesday: [
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 64800 },
+      ],
+      wednesday: [],
+      thursday: [
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 64800 },
+      ],
+      friday: [{ type: 'open', value: 36000 }],
+      saturday: [
+        { type: 'close', value: 3600 },
+        { type: 'open', value: 36000 },
+      ],
+      sunday: [
+        { type: 'close', value: 3600 },
+        { type: 'open', value: 43200 },
+        { type: 'close', value: 75600 },
+      ],
+    },
   ],
-  sunday: [
-    { type: 'close', value: 3600 },
-    { type: 'open', value: 43200 },
-    { type: 'close', value: 75600 },
+
+  [
+    'Multislot day',
+    {
+      monday: [],
+      tuesday: [
+        { type: 'open', value: 3600 },
+        { type: 'close', value: 7200 },
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 39800 },
+        { type: 'open', value: 43200 },
+        { type: 'close', value: 75600 },
+      ],
+    },
   ],
-};
+  [
+    'One more multi slot and next day close timetable',
+    {
+      monday: [
+        { type: 'open', value: 3600 },
+        { type: 'close', value: 7200 },
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 39800 },
+        { type: 'open', value: 43200 },
+        { type: 'close', value: 75600 },
+      ],
+      tuesday: [
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 64800 },
+      ],
+      wednesday: [
+        { type: 'open', value: 37800 },
+        { type: 'close', value: 41400 },
+      ],
+      thursday: [
+        { type: 'open', value: 36000 },
+        { type: 'close', value: 64800 },
+      ],
+      friday: [{ type: 'open', value: 36000 }],
+      saturday: [
+        { type: 'close', value: 3600 },
+        { type: 'open', value: 36000 },
+      ],
+      sunday: [
+        { type: 'close', value: 3600 },
+        { type: 'open', value: 43200 },
+        { type: 'close', value: 75600 },
+      ],
+    },
+  ],
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>Timetable Examples</p>
       </header>
-      <TimeTable timetable={timetable} />
+      <div className="App-content">
+        {timetables.map(([title, timetable]) => (
+          <div>
+            <h3>{title}</h3>
+            <TimeTable key={title} timetable={timetable} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
